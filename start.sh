6ff0609adcaed7059cb2bc359d83db94e31edf63
SOJEONG <69558545+sso-jeong@users.noreply.github.com>
2025-06-14 18:42:35 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,5 +10,9 @@ sleep 5
 echo "✅ Starting Kafka..."
 /opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties > /tmp/kafka.log 2>&1 &
 
+# 🔹 HTTP 서버로 Cloudtype의 Health Check 통과
+echo "✅ Starting dummy HTTP server on 9094..."
+python3 -m http.server 9094 > /tmp/http.log 2>&1
+
 # Kafka 로그 계속 출력해서 죽었는지 확인
 tail -f /tmp/kafka.log

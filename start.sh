5a1125fac438e0d6cc2e9761db63bb8d01d2a124
SOJEONG <69558545+sso-jeong@users.noreply.github.com>
2025-06-14 18:35:04 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,11 +4,11 @@
 echo "✅ Starting Zookeeper..."
 /opt/kafka/bin/zookeeper-server-start.sh /opt/kafka/config/zookeeper.properties > /tmp/zookeeper.log 2>&1 &
 
-# Kafka 실행
 sleep 5
+
+# Kafka 실행
 echo "✅ Starting Kafka..."
 /opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties > /tmp/kafka.log 2>&1 &
 
-# 🔹 HTTP 서버로 Cloudtype의 Health Check 통과
-echo "✅ Starting dummy HTTP server on 3000..."
-python3 -m http.server 3000 > /tmp/http.log 2>&1
+# Kafka 로그 계속 출력해서 죽었는지 확인
+tail -f /tmp/kafka.log
